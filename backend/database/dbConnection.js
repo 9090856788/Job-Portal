c1de287440ef7b0777725afc50be3dbcaa3ddd6b
9090856788 <kanhucharansahoo595@gmail.com>
2025-03-14 13:21:10 +0530

    remove db URL

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Kanhu143:Kanhu143@job-portal.miay9.mongodb.net/JobPortalDB?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Database Connected Successfully");
   } catch (error) {
     console.error("❌ Database Connection Failed:", error.message);
